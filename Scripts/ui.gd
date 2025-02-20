4463edfd77c31551abe818e26f3797f22d4ec089
AndresLopez28C <Jefreyalejandro28@gmail.com>
2025-02-20 17:28:01 -0500

    Cambios barra Estamina
    Se pone timer a barra de estamina

--- Scripts/ui.gd
+++ Scripts/ui.gd
@@ -1,7 +1,7 @@
 extends CanvasLayer
 @onready var estamina: ProgressBar = %Estamina
 signal cansancio
-
+var desactivado : bool =false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -9,11 +9,14 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_pressed("Correr"):
+	if Input.is_action_pressed("Correr") and !desactivado:
 		estamina.value -= 20* delta
 	else:
 		estamina.value += 10* delta
 	if estamina.value ==0:
 		cansancio.emit()
+		desactivado = true
+	if estamina.value ==100:
+		desactivado = false
 func _input(event):
 	pass
